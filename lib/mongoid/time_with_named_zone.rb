870d26ca59a5321c867f498509bd2de3b6ff9096
Caleb Tutty <caleb@carnivallabs.com>
2014-10-20 11:33:48 +1300

    Added version and refactored name space

---
+++ lib/mongoid/time_with_named_zone.rb
@@ -0,0 +1,50 @@
+require 'mongoid/time_with_named_zone/version'
+
+# Mongoid namespace from the Mongoid gem
+module Mongoid
+  # Object that stores time and tz information as embedded object
+  # transparently works with AS:TwZ, supports mongoid serialization
+  class TimeWithNamedZone
+    attr_reader :time, :zone
+
+    def initialize(time, zone = 'UTC')
+      @time, @zone = time, zone
+    end
+
+    # Converts an object of this instance into a database friendly value.
+    def mongoize
+      TimeWithNamedZone.mongoize(self)
+    end
+
+    class << self
+      # Get the object as it was stored in the database, and instantiate`
+      # this custom class from it.
+      def demongoize(object)
+        object[:time].in_time_zone(object[:zone]) if object
+      end
+
+      def mongoize(object)
+        hash = case object
+               when self
+                 {
+                   time: object.time,
+                   zone: ActiveSupport::TimeZone.new(object.zone).name
+                 }
+               when ActiveSupport::TimeWithZone
+                 {
+                   time: object.utc,
+                   zone: object.time_zone
+                 }
+               when Time
+                 {
+                   time: object.utc,
+                   zone: 'UTC'
+                 }
+               else
+                 nil
+               end
+        hash.mongoize
+      end
+    end
+  end
+end
