e165d4ee6080113103c6ca87190feccb2ed8c0dd
Caleb Tutty <caleb@carnivallabs.com>
2014-10-20 11:56:42 +1300

    Updated dependencies and spec namespace

--- Gemfile
+++ Gemfile
@@ -1,8 +1,3 @@
 source 'https://rubygems.org'
 
-gem 'mongoid'
-
-group :test do
-  gem 'pry'
-  gem 'rspec', '~> 3.1.0'
-end
+gemspec
--- spec/time_with_zone_spec.rb
+++ spec/time_with_zone_spec.rb
@@ -1,19 +1,18 @@
-require 'time_with_zone'
 require 'mongoid'
-require 'pry'
+require 'mongoid/time_with_named_zone'
 
-describe 'TimeWithZone' do
+describe Mongoid::TimeWithNamedZone do
   describe '#mongoize' do
-    context 'TimeWithZone' do
-      let(:time) { TimeWithZone.new(Time.utc(2010, 11, 19), 'Pacific/Auckland') }
-      let(:time_with_invalid_zone) { TimeWithZone.new(Time.utc(2010, 11, 19), 'Invalid_timezone') }
+    context 'TimeWithNamedZone' do
+      let(:time) { Mongoid::TimeWithNamedZone.new(Time.utc(2010, 11, 19), 'Pacific/Auckland') }
+      let(:time_with_invalid_zone) { Mongoid::TimeWithNamedZone.new(Time.utc(2010, 11, 19), 'Invalid_timezone') }
 
       it 'returns hash with utc time and zone' do
-        expect(TimeWithZone.mongoize(time)).to eq(time: Time.utc(2010, 11, 19), zone: 'Pacific/Auckland')
+        expect(Mongoid::TimeWithNamedZone.mongoize(time)).to eq(time: Time.utc(2010, 11, 19), zone: 'Pacific/Auckland')
       end
 
       it 'raise an error for invalid timezone' do
-        expect { TimeWithZone.mongoize(time_with_invalid_zone) }.to raise_error
+        expect { Mongoid::TimeWithNamedZone.mongoize(time_with_invalid_zone) }.to raise_error
       end
     end
 
@@ -21,7 +20,7 @@ describe 'TimeWithZone' do
       let(:time) { ActiveSupport::TimeWithZone.new(Time.utc(2010, 11, 19), 'Pacific/Auckland') }
 
       it 'returns hash with utc time and zone' do
-        expect(TimeWithZone.mongoize(time)).to eq(time: Time.utc(2010, 11, 19), zone: 'Pacific/Auckland')
+        expect(Mongoid::TimeWithNamedZone.mongoize(time)).to eq(time: Time.utc(2010, 11, 19), zone: 'Pacific/Auckland')
       end
     end
 
@@ -29,7 +28,7 @@ describe 'TimeWithZone' do
       let(:time) { Time.utc(2010, 11, 19) }
 
       it 'returns hash with utc time and utc zone' do
-        expect(TimeWithZone.mongoize(time)).to eq(time: Time.utc(2010, 11, 19), zone: 'UTC')
+        expect(Mongoid::TimeWithNamedZone.mongoize(time)).to eq(time: Time.utc(2010, 11, 19), zone: 'UTC')
       end
     end
   end
@@ -37,12 +36,12 @@ describe 'TimeWithZone' do
   describe '.demongoize' do
     it 'returns time in saved zone' do
       hash = { time: Time.utc(2010, 11, 19), zone: 'Pacific/Auckland' }
-      demongoized_value = TimeWithZone.demongoize(hash)
+      demongoized_value = Mongoid::TimeWithNamedZone.demongoize(hash)
       expect(demongoized_value).to eq(Time.new(2010, 11, 19, '+13:00'))
     end
 
     it 'returns nil for blank object' do
-      expect(TimeWithZone.demongoize(nil)).to eq(nil)
+      expect(Mongoid::TimeWithNamedZone.demongoize(nil)).to eq(nil)
     end
   end
 end
